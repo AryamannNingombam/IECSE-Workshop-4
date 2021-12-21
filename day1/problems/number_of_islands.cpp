14f7af929e4644acfbd55b35f51467cf8bd13e03
Anurag Chowdhury <canurag18212@gmail.com>
2021-12-21 15:39:38 +0530

    Small change

--- day1/problems/number_of_islands.cpp
+++ day1/problems/number_of_islands.cpp
@@ -1,17 +1,16 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
-    int dfs(vector<vector<char>>& grid,int i,int j,int m,int n)
+    void dfs(vector<vector<char>>& grid,int i,int j,int m,int n)
     {
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0') //Boundary check (0 is water) (Rest are Matrix boundaries)
-            return 0;
+            return;
         grid[i][j]='0'; //Visiting island (Can be considered as sunken as we visit it)
         //4 Recursive DFS call for traverals across all the 4 directions discussed in the question
         dfs(grid,i+1,j,m,n); // Check down
         dfs(grid,i-1,j,m,n); // Check up
         dfs(grid,i,j+1,m,n); // Check right
         dfs(grid,i,j-1,m,n); // Check left
-        return 1;
     }
     int numIslands(vector<vector<char>>& grid) {
         int m=grid.size(),n=grid[0].size();
@@ -23,7 +22,8 @@ public:
             {
                 //Spotting islands 
                 if(grid[i][j]=='1')
-                ctr+=dfs(grid,i,j,m,n); //Calling the DFS function the moment we find a possible island
+                ctr+=1;
+                dfs(grid,i,j,m,n); //Calling the DFS function the moment we find a possible island
             }
         }
         //Return counter after finding all the possible isolated islands
